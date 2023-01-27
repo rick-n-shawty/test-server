4261d33c34fb3d9a1bcb8ad85c43e26e0e06494d
rick-n-shawty <112240798+rick-n-shawty@users.noreply.github.com>
2023-01-27 12:05:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://test-react-1mlb.onrender.com',
     credentials: true
 }))
 const db = [{id: 0, name: 'sam', password: '123'}]
